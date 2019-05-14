b5e04f5f3329fe2ee41be8a20210dca01ead02c7
abdallahk <34738295+abdallahk@users.noreply.github.com>
2019-05-14 14:21:40 +0400

    6th

--- footer.php
+++ footer.php
@@ -1,14 +1,18 @@
-<footer class="footer is-medium">
-  <div class="container">
-  	<div class="columns">
-  	<?php dynamic_sidebar( 'footer-1' ); ?>
-  	<?php dynamic_sidebar( 'footer-2' ); ?>
-  	<?php dynamic_sidebar( 'footer-3' ); ?>
-  	<?php dynamic_sidebar( 'footer-4' ); ?>
-  	</div>
+<footer class="footer is-medium" style="padding: 1em;">
+	<div class="container">
+		<div class="columns">
+			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-2' ); ?></div>
+			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+			<div class="column is-one-quarter"><?php dynamic_sidebar( 'footer-4' ); ?></div>
+		</div>
 
-     <div><?=get_theme_mod( 'footer_copyright_text' )?></div>
-  </div>
+		<div class="columns">
+			<div class="column">
+				<div><?=get_theme_mod( 'footer_copyright_text' )?></div>
+			</div>
+		</div>
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
